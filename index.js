28c881e5262912b6eb83069a75a684a70cf746d8
JenningsF <6432715+JenningsF@users.noreply.github.com>
2021-09-28 07:13:20 -0500

    Finished basic CRUD implementation and added method-override node package.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const { executionAsyncResource } = require("async_hooks");
 
 main()
 	.then(() => {
@@ -21,9 +23,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.listen(3000, () => {
-	console.log("Serving on port 3000");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
 	res.render("home");
@@ -34,7 +35,40 @@ app.get("/campgrounds", async (req, res) => {
 	res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+	res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	const campground = await Campground.findByIdAndUpdate(id, {
+		...req.body.campground,
+	});
+	res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id);
+	res.redirect("/campgrounds");
+});
+
+app.listen(3000, () => {
+	console.log("Serving on port 3000");
 });
