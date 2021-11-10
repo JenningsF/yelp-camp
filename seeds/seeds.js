cc3dfc874b5e059534573fcdbccbe36a2614c1be
JenningsF <6432715+JenningsF@users.noreply.github.com>
2021-11-10 00:07:48 -0600

    Added styling for all views

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -22,9 +22,14 @@ const seedDatabase = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const randomNum = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: "https://source.unsplash.com/collection/483251",
+			description:
+				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat eaque recusandae ipsam ut. Sit sint deserunt hic illum dolore libero, autem vel culpa distinctio nobis aliquam rem tempore atque? Quas.",
+			price,
 		});
 		await camp.save();
 	}
