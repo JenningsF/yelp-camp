5a9f809c111850f56e35967d69eb3a146bb5f1f1
JenningsF <6432715+JenningsF@users.noreply.github.com>
2022-07-17 22:18:30 -0500

    Added routes and flash messages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
 const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
@@ -11,6 +13,9 @@ const Review = require("./models/review");
 const { executionAsyncResource } = require("async_hooks");
 const { STATUS_CODES } = require("http");
 const review = require("./models/review");
+
+const campgrounds = require("./routes/camgrounds");
+const reviews = require("./routes/reviews");
 
 main()
 	.then(() => {
@@ -33,6 +38,21 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Creates cookie for session
+const sessionConfig = {
+	secret: "thisshouldbeabettersecret", // Dummy secret for dev
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		HttpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
+}
+app.use(session(sessionConfig));
+app.use(flash());
 
 const validateCampground = (req, res, next) => {
 	const { error } = campgroundSchema.validate(req.body);
@@ -56,65 +76,20 @@ const validateReview = (req, res, next) => {
 	}
 }
 
+// Flash alerts for successes and errors
+app.use((req, res, next) => {
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+// Routes for campgrounds and reviews
+app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
-
-app.get("/campgrounds", catchAsync(async (req, res) => {
-	const campgrounds = await Campground.find({});
-	res.render("campgrounds/index", { campgrounds });
-}));
-
-app.get("/campgrounds/new", (req, res) => {
-	res.render("campgrounds/new");
-});
-
-app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
-	const campground = new Campground(req.body.campground);
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.get("/campgrounds/:id", catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id).populate("reviews");
-	res.render("campgrounds/show", { campground });
-}));
-
-app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/edit", { campground });
-}));
-
-app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
-	const { id } = req.params;
-	const campground = await Campground.findByIdAndUpdate(id, {
-		...req.body.campground,
-	});
-	res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
-	const { id } = req.params;
-	await Campground.findByIdAndDelete(id);
-	res.redirect("/campgrounds");
-}));
-
-app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
-	const review = new Review(req.body.review);
-	campground.reviews.push(review);
-	await review.save();
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-	console.log(req.params);
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
 
 app.all("*", (req, res, next) => {
 	next(new ExpressError("Page Not Found", 404));
