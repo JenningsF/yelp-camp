1ceb5112194d8e697cc8fb8532edd72dc6e7e33f
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-04-19 12:32:09 -0500

    Modified seeds to have National Parks instead of cities

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const cities = require("./cities");
+const cities = require("./cities");												// Uncomment if using cities instead of parks
+const parks = require("./parks");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
@@ -20,10 +21,11 @@ async function main() {
 
 const seedDatabase = async () => {
 	await Campground.deleteMany({});
-	for (let i = 0; i < 200; i++) {
-		const randomNum = Math.floor(Math.random() * 1000);
+	for (let i = 0; i < 100; i++) {
+		const randomNum = Math.floor(Math.random() * parks.length);
+		// const randomNum = Math.floor(Math.random() * cities.length);			// Uncomment if using cities instead of parks
 		const price = Math.floor(Math.random() * 20) + 10;
-		const { city, state, longitude, latitude } = cities[randomNum];
+		// const { city, state, longitude, latitude } = cities[randomNum];		// Uncomment if using cities instead of parks
 		const camp = new Campground({
 			title: `${sample(descriptors)} ${sample(places)}`,
 			images: [
@@ -32,14 +34,16 @@ const seedDatabase = async () => {
 					filename: "YelpCamp/uj8vnaw9iwbyg2uzrxln",
 				},
 			],
-			geometry: {
-				type: "Point",
-				coordinates: [`${longitude}`, `${latitude}`],
-			},
+			// geometry: {														// Uncomment if using cities instead of parks
+			// 	type: "Point",
+			// 	coordinates: [`${longitude}`, `${latitude}`],
+			// },
+			geometry: parks[randomNum].geometry,
 			price,
 			description:
 				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat eaque recusandae ipsam ut. Sit sint deserunt hic illum dolore libero, autem vel culpa distinctio nobis aliquam rem tempore atque? Quas.",
-			location: `${city}, ${state}`,
+			location: parks[randomNum].properties.Name,
+			// location: `${city}, ${state}`,									// Uncomment if using cities instead of parks
 			author: "6439bcaacb9542b05c018149",
 		});
 		await camp.save();
